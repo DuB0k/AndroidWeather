67c12d2d87b1147be4b7acfd58113d13252c992f
Pablo Castellanos <dubok@MacBookAir.local>
2014-12-18 13:37:04 +0100

    Lunch break commit

---
+++ app/src/main/java/com/talentum/dubok/weatherworkd/model/Cities.java
@@ -0,0 +1,40 @@
+package com.talentum.dubok.weatherworkd.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cities {
+
+    //Las variables de instancia como Cities solo se usan dentro del constructor, el getter
+    //o el setter.
+    private List<City> cities;
+
+    public Cities(){
+
+    }
+
+    //LAZY GETTER: se crean las varibles en el ultimo momento
+    public List<City> getCities() {
+        if (cities == null){
+            cities = new ArrayList<City>();
+        }
+        return cities;
+    }
+
+    public int size(){
+        return getCities().size();
+    }
+
+    public void addCity(City city){
+        getCities().add(city);
+    }
+
+    public void deleteCityAtIndex(int idx){
+        getCities().remove(idx);
+    }
+
+    public List allCities(){
+        return getCities();
+    }
+
+}
--- app/src/main/java/com/talentum/dubok/weatherworkd/model/City.java
+++ app/src/main/java/com/talentum/dubok/weatherworkd/model/City.java
@@ -3,7 +3,19 @@ package com.talentum.dubok.weatherworkd.model;
 
 
 public class City {
+    private String name;
 
+    public City(String name){
+        if (name == null || name == ""){
+            name = "Madrid";
+        }
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 }
