de84c608653be11327cb8382e3fd5d25cb2e6265
Pablo Castellanos <dubok@MacBookAir.local>
2014-12-18 13:12:21 +0100

    FizzBuzz Tests completed

--- app/src/androidTest/java/com/talentum/dubok/weatherworkd/FizzBuzzTests.java
+++ app/src/androidTest/java/com/talentum/dubok/weatherworkd/FizzBuzzTests.java
@@ -6,8 +6,10 @@ import com.talentum.dubok.weatherworkd.model.FizzBuzz;
 /*
 * TDD TestDrivenDevelopment
 * RedGreenRefactoring
-* Hacer un test que falle, ejecutar, ver errores, codificar para que pase el test y ejecutar
-* */
+* Hacer un test que falle, ejecutar, ver errores, codificar para que pase el test haciendo refactor
+* y ejecutar de nuevo Refactor es hacer cambios sin tocar la interfaz externa
+* Hacer test es un 20% extra del tiempo de desarrollo del proyecto
+***/
 
 public class FizzBuzzTests  extends AndroidTestCase{
 
@@ -17,10 +19,22 @@ public class FizzBuzzTests  extends AndroidTestCase{
         assertEquals("1",returnValue);
     }
 
-    public void testFizzBuzzWith3(){
+    public void testFizzBuzzReturnsBuzzWith3(){
         String returnValue = FizzBuzz.fizzBuzz(3);
 
         assertEquals("Fizz",returnValue);
+    }
+
+    public void testFizzBuzzReturnsBuzzWith5(){
+        String returnValue = FizzBuzz.fizzBuzz(5);
+
+        assertEquals("Buzz",returnValue);
+    }
+
+    public void testFizzBuzzReturnsFizzBuzzWith15(){
+        String returnValue = FizzBuzz.fizzBuzz(15);
+
+        assertEquals("FizzBuzz",returnValue);
     }
 
 }
--- app/src/main/java/com/talentum/dubok/weatherworkd/model/FizzBuzz.java
+++ app/src/main/java/com/talentum/dubok/weatherworkd/model/FizzBuzz.java
@@ -3,10 +3,28 @@ package com.talentum.dubok.weatherworkd.model;
 
 public class FizzBuzz {
     public static String fizzBuzz(int i) {
-        if (i==3){
+
+        if (isDivisibleByThree(i) && isDivisibleByFive(i)){
+            return "FizzBuzz";
+        }
+
+        if (isDivisibleByThree(i)){
             return "Fizz";
         }
-        return "1";
 
+        if (isDivisibleByFive(i)){
+            return "Buzz";
+        }
+
+        return "1";
     }
+
+    public static boolean isDivisibleByThree(int i){
+        return i % 3 == 0;
+    }
+
+    public static boolean isDivisibleByFive(int i){
+        return i % 5 == 0;
+    }
+
 }
